41ff9e13aa493aa76537b8ea51e8640454b6f83b
Danton Lucas Ribeiro Rodrigues <Dantonlucasrr@gmail.com>
2023-12-03 23:49:34 -0300

    exemplo de modal na tela de detalhes

--- app.js
+++ app.js
@@ -18,11 +18,48 @@
 //     });
 
 
+// const urlCachoeiras = 'https://jsonserver-cachoeiras--dantonlucassaga.repl.co/Cachoeiras';
+// const urlDestaques = 'https://jsonserver-cachoeiras--dantonlucassaga.repl.co/Destaques';
+// const urlfotos = 'https://jsonserver-cachoeiras.dantonlucassaga.repl.co/Fotos';
+   
+// let Cachoeiras = [];
+// let Destaques = [];
+// let Fotos = [];
+// function carregarDados() {
+//     const promiseCachoeiras = fetch(urlCachoeiras)
+//         .then(response => response.json())
+//         .then(function (dados) {
+//             Cachoeiras = dados;
+//             // console.log('Dados de Cachoeiras Carregados!');
+//         });
+
+//     const promiseDestaques = fetch(urlDestaques)
+//         .then(response => response.json())
+//         .then(function (dados) {
+//             Destaques = dados;
+//             // console.log('Dados de Destaques Carregados!');
+//         });
+
+//     return Promise.all([promiseCachoeiras, promiseDestaques]);
+// }
+
+// carregarDados()
+//     .then(() => {
+//         console.log('Cachoeiras:', Cachoeiras);
+//         console.log('Destaques:', Destaques);
+//     })
+//     .catch(error => {
+//         console.error('Erro ao carregar dados:', error);
+//     });
+
+
 const urlCachoeiras = 'https://jsonserver-cachoeiras--dantonlucassaga.repl.co/Cachoeiras';
 const urlDestaques = 'https://jsonserver-cachoeiras--dantonlucassaga.repl.co/Destaques';
-
+const urlfotos = 'https://jsonserver-cachoeiras.dantonlucassaga.repl.co/Fotos';
+   
 let Cachoeiras = [];
 let Destaques = [];
+let Fotos = [];
 
 function carregarDados() {
     const promiseCachoeiras = fetch(urlCachoeiras)
@@ -39,16 +76,22 @@ function carregarDados() {
             // console.log('Dados de Destaques Carregados!');
         });
 
-    return Promise.all([promiseCachoeiras, promiseDestaques]);
+    const promiseFotos = fetch(urlfotos)
+        .then(response => response.json())
+        .then(function (dados) {
+            Fotos = dados;
+            // console.log('Dados de Fotos Carregados!');
+        });
+
+    return Promise.all([promiseCachoeiras, promiseDestaques, promiseFotos]);
 }
 
 carregarDados()
     .then(() => {
         console.log('Cachoeiras:', Cachoeiras);
         console.log('Destaques:', Destaques);
+        console.log('Fotos:', Fotos);
     })
     .catch(error => {
         console.error('Erro ao carregar dados:', error);
     });
-
-
